1c133293ac573a485c64503431c1e1d97e78840f
Jiri <wi22b004@technikum-wien.at>
2024-10-24 17:37:00 +0200

    Tasklist Component created and added to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,19 @@
   <template>
+    <Tasklist />
   </template>
 
   <script>
   import { defineComponent } from 'vue';
 
   // Components
+  import Tasklist from '@/components/Tasklist.vue';
+
 
   export default defineComponent({
     name: 'HomeView',
+    components: {
+      Tasklist // Tasklist-Komponente registrieren
+    }
 
   });
   </script>
