3cef894fe8e2263fc89602427a88c9553616283a
Florentin Holzer <wi22b090@fh-technikum.at>
2024-12-04 18:48:46 +0100

    updated UI and added Badges

---
+++ src/components/Tasks/Badge.vue
@@ -0,0 +1,85 @@
+<script setup>
+defineProps(['BadgeTitle', 'BadgeDescription']);
+</script>
+
+<template>
+    <div class="flip-card mx-1 my-1">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <p class="title">{{ BadgeTitle}}</p>
+                <v-icon class="medal">mdi-medal</v-icon>
+            </div>
+            <div class="flip-card-back">
+                <p class="desceription">{{ BadgeDescription }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/* From Uiverse.io by joe-watson-sbf */
+.flip-card {
+    background-color: transparent;
+    width: 110px;
+    height: 150px;
+    perspective: 1000px;
+    font-family: sans-serif;
+}
+
+.title {
+    font-size: 1.3em;
+    font-weight: 900;
+    text-align: center;
+    margin: 0;
+}
+
+.medal {
+    font-size: 3em;
+    color: goldenrod;
+    margin: 0 auto;
+}
+
+.desceription {
+    font-size: 1em;
+    font-weight: 500;
+    text-align: center;
+    margin: 0;
+}
+
+.flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+    box-shadow: 0 8px 14px 0 rgba(0,0,0,0.2);
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    border: 1px solid goldenrod;
+    border-radius: 1rem;
+}
+
+.flip-card-front {
+    background: gold;
+}
+
+.flip-card-back {
+    background: goldenrod;
+    color: black;
+    transform: rotateY(180deg);
+}
+</style>
--- src/components/Tasks/Tasklist.vue
+++ src/components/Tasks/Tasklist.vue
@@ -1,91 +1,107 @@
 <template>
-  <v-container fluid class="d-flex justify-center align-center">
-    <v-col cols="12" md="6">
-      <v-card class="pa-4">
-        <h1>STUDYMASTER</h1>
-        <v-divider class="mb-4"></v-divider>
-        <!-- Fortschrittsanzeige für ECTS-Punkte -->
-        <ProgressBar :completedEcts="0"/>
+    <v-container fluid class="d-flex justify-center align-center">
+        <v-col cols="12" md="6">
+            <v-card class="main pa-4">
+                <h1>STUDYMASTER</h1>
+                <v-divider class="mb-4"></v-divider>
+                <!-- Fortschrittsanzeige für ECTS-Punkte -->
+                <ProgressBar :completedEcts="0"/>
 
-        <v-row class="mt-4">
-          <v-col>
-            <BigOrange @click="addTask" text="ADD TASK"/>
-          </v-col>
+                <v-row class="mt-4">
+                    <v-col>
+                        <BigGreen @click="addTask" text="ADD TASK"/>
+                    </v-col>
+                    <v-divider class="mr-4" vertical></v-divider>
+                    <v-col>
+                        <v-row>
+                            <Badge
+                                BadgeTitle="Badge1"
+                                BadgeDescription="This is the Badge description."
+                            />
+                            <Badge
+                                BadgeTitle="Badge2"
+                                BadgeDescription="This is the Badge description."
+                            />
+                            <Badge
+                                BadgeTitle="Badge3"
+                                BadgeDescription="This is the Badge description."
+                            />
+                        </v-row>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <!-- Task-Grid -->
+                    <v-divider class="mt-2"></v-divider>
+                    <i style="color: gray;" v-if="tasks.length===0">No tasks created yet...</i>
 
-          <v-col>
-            <v-card>
-              <h1>Badges</h1>
+                    <div class="task-grid mx-6 my-6">
+                        <Task
+                            v-for="(task) in tasks"
+                            :task-id="task.id"
+                            :subject="task.title"
+                            :ects="task.ects"
+                            :description="task.description"
+                            :due_date="task.dueDate"
+                            :onDelete="deleteTask"
+                        />
+                    </div>
+                </v-row>
             </v-card>
-          </v-col>
-        </v-row>
-      </v-card>
-
-      <!-- Task-Grid -->
-      <div class="task-grid mt-6">
-        <Task
-            v-for="(task) in tasks"
-            :task-id="task.id"
-            :subject="task.title"
-            :ects="task.ects"
-            :description="task.description"
-            :due_date="task.dueDate"
-            :onDelete="deleteTask"
-        />
-      </div>
-    </v-col>
-  </v-container>
+        </v-col>
+    </v-container>
 </template>
 
 <script>
 import Task from "@/components/Tasks/Task.vue";
 import ProgressBar from "@/components/Tasks/ProgressBar.vue";
-import BigOrange from "@/components/Buttons/BigOrange.vue";
+import BigGreen from "@/components/Buttons/BigGreen.vue";
+import Badge from "@/components/Tasks/Badge.vue";
 
 export default {
-  name: "Tasklist",
-  components: {BigOrange, ProgressBar, Task},
-  beforeMount() { // Lifecycle-Hook: Wird vor dem Rendern ausgeführt
-    this.fetchTasks();
-  },
-  updated() {
-    this.fetchTasks();
-  },
-  computed: {
-    tasks() {
-      return this.$store.state.tasks;
+    name: "Tasklist",
+    components: {Badge, BigGreen, ProgressBar, Task},
+    beforeMount() { // Lifecycle-Hook: Wird vor dem Rendern ausgeführt
+        this.fetchTasks();
     },
-  },
-  methods: {
-    async fetchTasks() {
-      await this.$store.dispatch('fetchTasks');
+    updated() {
+        this.fetchTasks();
     },
+    computed: {
+        tasks() {
+            return this.$store.state.tasks;
+        },
+    },
+    methods: {
+        async fetchTasks() {
+            await this.$store.dispatch('fetchTasks');
+        },
 
-    async addTask() {
-      await this.$store.dispatch('addTask', {
-        user_id: this.$store.state.user.id,
-        title: "New Task",
-        ects: 5,
-        description: "Description",
-        dueDate: "2024-12-31",
-      });
+        async addTask() {
+            await this.$store.dispatch('addTask', {
+                user_id: this.$store.state.user.id,
+                title: "New Task",
+                ects: 5,
+                description: "Description",
+                dueDate: "2024-12-31",
+            });
+        },
+        async deleteTask(taskId) {
+            await this.$store.dispatch('deleteTask', taskId);
+        },
     },
-    async deleteTask(taskId) {
-      await this.$store.dispatch('deleteTask', taskId);
-    },
-  },
 };
 </script>
 
 <style scoped>
 .v-container {
-  margin-top: 64px;
-  margin-bottom: 120px;
+    margin-top: 64px;
+    margin-bottom: 120px;
 }
 
 .task-grid {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 16px;
-  justify-content: center;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 16px;
+    justify-content: center;
 }
 </style>
